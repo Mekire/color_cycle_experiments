e606aeb21438ecb02af68df74f9c5a11e9121571
Mekire <3ror@live.com>
2014-05-07 00:32:14 +0900

    Generalized to a class.

--- cycle.py
+++ cycle.py
@@ -1,12 +1,15 @@
 import os
 import sys
-
 import pygame as pg
 
 
-FALL_1 = [(32,136,168), (48,144,168), (72,152,176), (96,160,184), (120,168,192),
-          (136,176,200), (160,184,208), (184,192,216), (208,208,224)]
-CYCLE = {color:FALL_1[(i-1)%len(FALL_1)] for i,color in enumerate(FALL_1)}
+#These are the color cycles that will be used.
+LOW = [(32,136,168), (48,144,168), (72,152,176), (96,160,184), (120,168,192),
+       (136,176,200), (160,184,208), (184,192,216), (208,208,224)]
+
+HIGH = [(80, 128,152), (96,136,160), (56,120,144)]
+
+FOAM = [(184,192,216), (48,144,169)]
 
 
 class Control(object):
@@ -21,34 +24,58 @@ class Control(object):
         self.timer = 0.0
         self.done = False
         self.image = pg.image.load("mega.png").convert(8)
+        cycles = [Cycle(self.image.subsurface(0,507,498,93),(0,507),LOW),
+                  Cycle(self.image.subsurface(0,330,498,153), (0,330),
+                        HIGH, fps=10),
+                  Cycle(self.image.subsurface(0,483,498,12), (0,483), FOAM)]
+        self.cycles = pg.sprite.Group(cycles)
 
     def event_loop(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.done = True
 
-    def cycle(self):
-        palette = list(self.image.get_palette())
-        for i,color in enumerate(palette):
-            palette[i] = pg.Color(*CYCLE.get(color[:3], color))
-        self.image.set_palette(palette)
-
     def update(self):
-        now = pg.time.get_ticks()
-        if now-self.timer > 1000.0/self.cycle_fps:
-            self.cycle()
-            self.timer = now
+        self.cycles.update(pg.time.get_ticks())
 
     def draw(self):
-        self.screen.blit(self.image, (0,0))
+        self.cycles.draw(self.screen)
 
     def main_loop(self):
+        self.screen.blit(self.image, (0,0))
         while not self.done:
             self.event_loop()
             self.update()
             self.draw()
             pg.display.update()
             self.clock.tick(self.fps)
+
+
+class Cycle(pg.sprite.Sprite):
+    def __init__(self, image, pos, color_cycle, fps=20, *groups):
+        pg.sprite.Sprite.__init__(self, *groups)
+        self.image = image
+        self.rect = self.image.get_rect(topleft=pos)
+        self.palette = list(self.image.get_palette())
+        self.cycle = self.make_cycle(color_cycle)
+        self.fps = fps
+        self.timer = pg.time.get_ticks()
+
+    def make_cycle(self, cycle):
+        return {color:cycle[(i-1)%len(cycle)] for i,color in enumerate(cycle)}
+
+    def advance_cycle(self):
+        for i,color in enumerate(self.palette):
+            self.palette[i] = pg.Color(*self.cycle.get(color[:3], color))
+        self.image.set_palette(self.palette)
+
+    def update(self, now):
+        if now-self.timer > 1000.0/self.fps:
+            self.advance_cycle()
+            self.timer = now
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
 
 
 if __name__ == "__main__":
